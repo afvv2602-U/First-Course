ea571ddff9361297f0689540b483ff276b67e3c8
Afvv2602 <adrian.fernandez@nexplayer.com>
2022-02-28 12:56:31 +0100

    Herencia update

--- POO/Herencia/Vehiculos.py
+++ POO/Herencia/Vehiculos.py
@@ -1,63 +1,63 @@
 class Vehiculo(object):
-    def __init__(this,color,num_ruedas,nombre):
-        this.color = color
-        this.num_ruedas = num_ruedas
-        this.nombre = nombre
+    def __init__(self,color,num_ruedas,nombre):
+        self.color = color
+        self.num_ruedas = num_ruedas
+        self.nombre = nombre
 
-    def __str__(this):
-        return 'Color {0}, Tiene {1} ruedas, se llama {2} '.format(this.color,this.num_ruedas,this.nombre)
+    def __str__(self):
+        return 'Color {0}, Tiene {1} ruedas, se llama {2} '.format(self.color,self.num_ruedas,self.nombre)
         
 class Coche(Vehiculo):
-    def __init__(this, color, num_ruedas, nombre,velocidad,cilindrada):
+    def __init__(self, color, num_ruedas, nombre,velocidad,cilindrada):
         super().__init__(color,num_ruedas,nombre)
-        this.velocidad = velocidad
-        this.cilindrada = cilindrada
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
-    def __str__(this):
-        return super().__str__() + "{0} km/h {1} cc ".format(this.velocidad,this.cilindrada)
+    def __str__(self):
+        return super().__str__() + "{0} km/h {1} cc ".format(self.velocidad,self.cilindrada)
 
 class Tanque(Vehiculo):
-    def __init__(this, color, num_ruedas, nombre,velocidad,cilindrada,ocupantes):
+    def __init__(self, color, num_ruedas, nombre,velocidad,cilindrada,ocupantes):
         super().__init__(color, num_ruedas, nombre)
-        this.velocidad = velocidad
-        this.cilindrada = cilindrada
-        this.ocupantes = ocupantes
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+        self.ocupantes = ocupantes
 
-    def __str__(this):
-        return super().__str__() + '%i km/h %i cc %i ocupantes '%(this.velocidad,this.cilindrada,this.ocupantes)
+    def __str__(self):
+        return super().__str__() + '%i km/h %i cc %i ocupantes '%(self.velocidad,self.cilindrada,self.ocupantes)
 
 class Bicicleta(Vehiculo):
-    def __init__(this, color, num_ruedas, nombre,tipo,peso):
+    def __init__(self, color, num_ruedas, nombre,tipo,peso):
         super().__init__(color, num_ruedas, nombre)
-        this.tipo = tipo
-        this.peso = peso
+        self.tipo = tipo
+        self.peso = peso
     
-    def __str__(this):
-        return super().__str__() + ', tipo %s , peso %i '%(this.tipo, this.peso)
+    def __str__(self):
+        return super().__str__() + ', tipo %s , peso %i '%(self.tipo, self.peso)
 
 class Limusina(Coche):
-    def __init__(this, color, num_ruedas, nombre, velocidad, cilindrada,num_asientos,num_puertas):
+    def __init__(self, color, num_ruedas, nombre, velocidad, cilindrada,num_asientos,num_puertas):
         super().__init__(color, num_ruedas, nombre, velocidad, cilindrada)
-        this.num_asientos = num_asientos
-        this.num_puertas = num_puertas
+        self.num_asientos = num_asientos
+        self.num_puertas = num_puertas
 
-    def __str__(this):
-        return super().__str__() + ', numero de asientos %i, numero de puertas %i'%(this.num_asientos,this.num_puertas)
+    def __str__(self):
+        return super().__str__() + ', numero de asientos %i, numero de puertas %i'%(self.num_asientos,self.num_puertas)
 
 class Camion(Tanque):
-    def __init__(this, color, num_ruedas, nombre, velocidad, cilindrada, ocupantes,num_asientos,armas : bool):
+    def __init__(self, color, num_ruedas, nombre, velocidad, cilindrada, ocupantes,num_asientos,armas : bool):
         super().__init__(color, num_ruedas, nombre, velocidad, cilindrada, ocupantes)
-        this.num_asientos = num_asientos
-        this.armas = armas
+        self.num_asientos = num_asientos
+        self.armas = armas
 
-    def __str__(this):
-        return super().__str__() + ', numero de asientos %i, tiene armas %r'%(this.num_asientos,this.armas)
+    def __str__(self):
+        return super().__str__() + ', numero de asientos %i, tiene armas %r'%(self.num_asientos,self.armas)
 
 
 class Triciclo(Bicicleta):
-    def __init__(this, color, num_ruedas, nombre, tipo, peso,bocina : bool):
+    def __init__(self, color, num_ruedas, nombre, tipo, peso,bocina : bool):
         super().__init__(color, num_ruedas, nombre, tipo, peso)
-        this.bocina = bocina
+        self.bocina = bocina
 
-    def __str__(this):
-        return super().__str__() + ', tiene bocina %r'%(this.bocina)
+    def __str__(self):
+        return super().__str__() + ', tiene bocina %r'%(self.bocina)
